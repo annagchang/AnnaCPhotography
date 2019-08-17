4cf74468ea401d3aaeee527ed8666c4725aaf6b0
Anna Chang <Anna Chang>
2019-08-16 23:44:12 -0700

    side menu trial 1

--- AnnaCPhotography/ViewController.swift
+++ AnnaCPhotography/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var menuShowing = false
 
+    @IBOutlet var leadingC: NSLayoutConstraint!
+    @IBOutlet var trailingC: NSLayoutConstraint!
+    @IBOutlet var homeScreen: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func menuTapped(_ sender: Any) {
+        //if the hamburger menu is NOT visible, then move the ubeView back to where it used to be
+        if !menuShowing {
+            leadingC.constant = 150
+            //this constant is NEGATIVE because we are moving it 150 points OUTWARD and that means -150
+            trailingC.constant = -150
+        } else {
+            //if the hamburger menu IS visible, then move the ubeView back to its original position
+            leadingC.constant = 0
+            trailingC.constant = 0
+        }
+        
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations: {
+            self.view.layoutIfNeeded()
+        })
+        menuShowing = !menuShowing
+    }
 }
 
